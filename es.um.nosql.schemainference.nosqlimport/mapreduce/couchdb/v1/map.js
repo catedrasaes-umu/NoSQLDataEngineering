7e9a747d71ee3790579449b698a2c267e9894d1c
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-01-13 13:10:43 +0100

    Mejora al proceso de map-reduce para hacerlo independiente de la base de datos. Creo que así va a ser más interesante la implementación.

--- es.um.nosql.schemainference.nosqlimport/mapreduce/couchdb/v1/map.js
+++ es.um.nosql.schemainference.nosqlimport/mapreduce/couchdb/v1/map.js
@@ -1,65 +1,67 @@
 function(doc)
 {
 
-function flatten_schema (obj , interesting_keys)
-{
-    var retschema = '';
-
-    function _search_string_in_array(str, strArray) {
-        for (var j=0; j<strArray.length; j++) {
-            if (strArray[j].match(str)) return j;
-        }
-        return -1;
-    }
-
-    function _complex_obj(obj, output_keys)
+    function flatten_schema (obj , interesting_keys)
     {
-        var _retschema = '';
-        Object.keys(obj).sort().forEach(function (key) {
-            //if (!obj.hasOwnProperty(key))
-            //    return;
+        var retschema = '';
 
-            if (output_keys)
-            {
-                _retschema += key + ':';
-
-                if ((typeof interesting_keys !== 'undefined')
-                    && (-1 !== _search_string_in_array(key, interesting_keys)))
-                    _retschema += '"' + escape(obj[key]) + '"';
+        function _search_string_in_array(str, strArray) {
+            for (var j=0; j<strArray.length; j++) {
+                if (strArray[j].match(str)) return j;
             }
+            return -1;
+        }
 
-            _retschema += flatten_schema(obj[key]);
-        });
+        function _complex_obj(obj, output_keys)
+        {
+            var _retschema = '';
+            Object.keys(obj).sort().forEach(function (key) {
+                //if (!obj.hasOwnProperty(key))
+                //    return;
 
-        return _retschema;
-    };
+                if (output_keys)
+                {
+                    _retschema += key + ':';
 
-    // Array
-    if (obj instanceof Array)
-    {
-        retschema += '[' + _complex_obj(obj, false) + ']';
-    } // null
-    else if (obj === null)
-    {
-        retschema += '0';
-    } // Date
-    else if (obj instanceof Date)
-    {
-        retschema += 'd';
-    }// Object
-    else if ((typeof obj) === 'object')
-    {
-        retschema += '{' + _complex_obj(obj, true) + '}';
+                    if ((typeof interesting_keys !== 'undefined')
+                        && (-1 !== _search_string_in_array(key, interesting_keys)))
+                        _retschema += '"' + escape(obj[key]) + '"';
+                }
+
+                _retschema += flatten_schema(obj[key]);
+            });
+
+            return _retschema;
+        };
+
+        // Array
+        if (obj instanceof Array)
+        {
+            retschema += '[' + _complex_obj(obj, false) + ']';
+        } // null
+        else if (obj === null)
+        {
+            retschema += '0';
+        } // Date
+        else if (obj instanceof Date)
+        {
+            retschema += 'd';
+        }// Object
+        else if ((typeof obj) === 'object')
+        {
+            retschema += '{' + _complex_obj(obj, true) + '}';
+        }
+        // Other
+        else
+        {
+            // number, string, boolean
+            retschema += (typeof obj)[0];
+        }
+
+        return retschema;
     }
-    // Other
-    else
-    {
-        // number, string, boolean
-        retschema += (typeof obj)[0];
-    }
 
-    return retschema;
-}
+    var schema = flatten_schema(doc,['type']);
 
-  emit(flatten_schema(doc,['type']), doc._id);
+    emit(schema, {schema: schema, count: 1});
 }
--- es.um.nosql.schemainference.nosqlimport/mapreduce/couchdb/v1/reduce.js
+++ es.um.nosql.schemainference.nosqlimport/mapreduce/couchdb/v1/reduce.js
@@ -1,7 +1,15 @@
 function (key, values, rereduce)
 {
-	if (rereduce)
-		return values;
-	else
-		return values[0];
+    if (rereduce)
+	return values;
+    else
+    {
+        v = values.reduce(function (v1, v2)
+                          {
+                              return {schema: v1.schema,
+                                      count: v1.count + v2.count};
+                          },
+                          {schema: key, count: 0});
+	return v;
+    }
 }
