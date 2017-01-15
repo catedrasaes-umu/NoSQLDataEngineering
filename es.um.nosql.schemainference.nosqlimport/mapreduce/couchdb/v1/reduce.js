d99d752774778150123dbb3afbd6822bbf922a86
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-01-15 19:55:26 +0100

    Strictness in JS.

--- es.um.nosql.schemainference.nosqlimport/mapreduce/couchdb/v1/reduce.js
+++ es.um.nosql.schemainference.nosqlimport/mapreduce/couchdb/v1/reduce.js
@@ -1,10 +1,10 @@
 function (key, values)
 {
-    v = values.reduce(function (v1, v2)
-                      {
-                          return {schema: key,
-                                  count: v1.count + v2.count};
-                      },
-                      {schema: key, count: 0});
+    var v = values.reduce(function (v1, v2)
+                          {
+                              return {schema: key,
+                                      count: v1.count + v2.count};
+                          },
+                          {schema: key, count: 0});
     return v;
 }
