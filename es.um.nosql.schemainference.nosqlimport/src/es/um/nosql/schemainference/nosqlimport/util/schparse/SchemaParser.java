191c4845821f4930e8d06bac4d4fee3b29e21024
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-02-08 13:26:44 +0100

    Advance in the schema parser.

--- es.um.nosql.schemainference.nosqlimport/src/es/um/nosql/schemainference/nosqlimport/util/schparse/SchemaParser.java
+++ es.um.nosql.schemainference.nosqlimport/src/es/um/nosql/schemainference/nosqlimport/util/schparse/SchemaParser.java
@@ -1,14 +1,62 @@
 package es.um.nosql.schemainference.nosqlimport.util.schparse;
 
-public class SchemaParser<StateT,StreamT, R>
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class SchemaParser<State extends StateBase>
 {
-	// 
-	private char memory;
+	private State state;
 	
-	private StateT state;
+	private Map<Character, Function<State,State>> parsers;
 	
-	public SchemaParser(StateT state)
+	private void _init()
 	{
-		this.state =state;
+		parsers = new HashMap<>();
+		// Add the parser for ':'
+		addParser(':', s -> {s.skipInput(1); return s;});
 	}
+	
+	public SchemaParser(State state)
+	{
+		this.state = state;
+		_init();
+	}
+
+	public SchemaParser()
+	{
+		state = null;
+		_init();
+	}
+	
+	public SchemaParser<State> addParser(Character c, Function<State,State> p)
+	{
+		parsers.put(c,p);
+		return this;
+	}
+	
+	public StateBase getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public StateBase parse()
+	{
+		int c;
+		while ((c = state.peek()) != -1)
+		{
+			char q = (char) c;
+			
+			if (parsers.containsKey(q))
+				state = parsers.get(q).apply(state);
+			else
+				state.skipInput(1);
+		}
+		return null;	
+	}
+
+
 }
