2098e66a51d98dd073a744481192866fbd88a183
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-03-03 11:08:19 +0100

    Fix Mongodb Adapter.

--- es.um.nosql.schemainference.nosqlimport/src/es/um/nosql/schemainference/nosqlimport/util/MongoDBStreamAdapter.java
+++ es.um.nosql.schemainference.nosqlimport/src/es/um/nosql/schemainference/nosqlimport/util/MongoDBStreamAdapter.java
@@ -18,12 +18,13 @@ public class MongoDBStreamAdapter extends AbstractStreamAdapter
 		JsonParser parser = new JsonParser();
 
 		result = mapRedMap.entrySet().stream().reduce(Stream.empty(), (s,e) ->
-					StreamSupport.stream(e.getValue().spliterator(),false).map(doc ->
-					{
-						JsonObject jObj = (JsonObject)(parser).parse(doc.get("_id").toString());
-						jObj.addProperty("type", e.getKey());
-						return jObj;
-					}),
+					Stream.concat(s,
+							StreamSupport.stream(e.getValue().spliterator(),false).map(doc ->
+							{
+								JsonObject jObj = (JsonObject)(parser).parse(doc.get("_id").toString());
+								jObj.addProperty("type", e.getKey());
+								return jObj;
+							})),
 					Stream::concat);
 
 		return result;
