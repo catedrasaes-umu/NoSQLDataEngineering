fe6f8b909d6b9580cdbe94ee6c1fc617de7687b3
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-03-14 15:36:52 +0100

    Added a main2 to test the new decision tree.
    Changed ModelLoader to accept several packages and to be able to register file extensions.

--- es.um.nosql.schemainference/src/es/um/nosql/schemainference/util/emf/ModelLoader.java
+++ es.um.nosql.schemainference/src/es/um/nosql/schemainference/util/emf/ModelLoader.java
@@ -8,30 +8,72 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 public class ModelLoader<T>
 {
 	private ResourceSet resourceSet;
+	static Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 
 	static
 	{
-		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 		Map<String, Object> map = reg.getExtensionToFactoryMap();
 		map.put("xmi", new XMIResourceFactoryImpl());
 		map.put("nosqlschema", new XMIResourceFactoryImpl());
 	}
 
-	public ModelLoader(EPackage thePackage)
+	public ModelLoader()
 	{
 		resourceSet = new ResourceSetImpl();
-		resourceSet.getPackageRegistry().put(thePackage.getNsURI(), thePackage);
+		final ExtendedMetaData extendedMetaData = 
+			new BasicExtendedMetaData(resourceSet.getPackageRegistry());
+		resourceSet.getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA,
+		    extendedMetaData);
+	}
+	
+	public ModelLoader(EPackage thePackage)
+	{
+		this();
+		registerPackages(thePackage);
 	}
 
+	// builder
+	public ModelLoader<T> forPackages(EPackage... packages)
+	{
+		registerPackages(packages);
+		return this;
+	}
+
+	// builder
+	public ModelLoader<T> withExtension(String extension, Object rf)
+	{
+		registerExtension(extension,rf);
+		return this;
+	}
+	
+	public void registerExtension(String extension, Object resourceFactory)
+	{
+		reg.getExtensionToFactoryMap().put(extension, resourceFactory);
+	}
+	
+	public void registerPackages(EPackage... packages)
+	{
+		for (EPackage p : packages)
+			resourceSet.getPackageRegistry().put(p.getNsURI(), p);
+	}
+	
 	@SuppressWarnings("unchecked")
 	public T load(File file)
 	{
 		Resource r = resourceSet.getResource(URI.createFileURI(file.getAbsolutePath()), true);
 		return (T)r.getContents().get(0);
+	}
+
+	public ResourceSet getResourceSet()
+	{
+		return resourceSet;
 	}
 }
