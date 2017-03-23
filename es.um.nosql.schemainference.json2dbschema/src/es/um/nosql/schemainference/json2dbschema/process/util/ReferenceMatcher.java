ac04917946b1f6472a207fe04f07654d70edb778
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-03-23 13:54:49 +0100

    Remove unused import.

--- es.um.nosql.schemainference.json2dbschema/src/es/um/nosql/schemainference/json2dbschema/process/util/ReferenceMatcher.java
+++ es.um.nosql.schemainference.json2dbschema/src/es/um/nosql/schemainference/json2dbschema/process/util/ReferenceMatcher.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import org.apache.commons.lang3.tuple.Pair;
 
