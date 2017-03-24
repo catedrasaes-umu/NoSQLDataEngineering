198f0f5e65700735e59c7b68173a4bf64f426d97
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-03-24 01:10:15 +0100

    Indent of Javascript code.

--- es.um.nosql.schemainference.nosqlimport/mapreduce/mongodb/v1/map.js
+++ es.um.nosql.schemainference.nosqlimport/mapreduce/mongodb/v1/map.js
@@ -1,20 +1,20 @@
 function()
 {
-	function flatten_schema_str(obj, interesting_keys)
-	{
-		return JSON.stringify(flatten_schema(obj,interesting_keys));
-	}
-	
+    function flatten_schema_str(obj, interesting_keys)
+    {
+	return JSON.stringify(flatten_schema(obj,interesting_keys));
+    }
+
     function flatten_schema (obj , interesting_keys)
     {
         var retschema = null;
 
         function _search_string_in_array(str, strArray)
         {
-        	return !strArray.every(function (s)
-            {
-        		return !s.toLowerCase().match(str);
-        	});
+            return !strArray.every(function (s)
+                                   {
+        		               return !s.toLowerCase().match(str);
+        	                   });
         }
 
         function _complex_obj(obj, asObject)
@@ -24,22 +24,23 @@ function()
             	_retschema = {};
             else
             	_retschema = [];
-            
-            Object.keys(obj).sort().forEach(function (key)
-            {
-                //if (!obj.hasOwnProperty(key))
-                //    return;
-                if (asObject)
-                {
-                    if ((typeof interesting_keys !== 'undefined')
-                        && _search_string_in_array(key, interesting_keys))
-                        _retschema[key] = obj[key];
-                    else
-                    	_retschema[key] = flatten_schema(obj[key]);
-                }
-                else
-                	_retschema.push(flatten_schema(obj[key]));
-            });
+
+            Object.keys(obj).sort()
+                .forEach(function (key)
+                         {
+                             //if (!obj.hasOwnProperty(key))
+                             //    return;
+                             if (asObject)
+                             {
+                                 if ((typeof interesting_keys !== 'undefined')
+                                     && _search_string_in_array(key, interesting_keys))
+                                     _retschema[key] = obj[key];
+                                 else
+                    	             _retschema[key] = flatten_schema(obj[key]);
+                             }
+                             else
+                	         _retschema.push(flatten_schema(obj[key]));
+                         });
 
             return _retschema;
         };
@@ -47,19 +48,19 @@ function()
         // Array
         if (Array.isArray(obj))
         {
-        	if (obj.length == 0)
-        		retschema = [];
-        	else
-        	{
-        		// See if we can produce just one array object with one inside type (homogeneous)
-        		var schemas = obj.map(function (e) { return flatten_schema(e); });
+            if (obj.length == 0)
+        	retschema = [];
+            else
+            {
+        	// See if we can produce just one array object with one inside type (homogeneous)
+        	var schemas = obj.map(function (e) { return flatten_schema(e); });
 
-        		var str_schema_0 = JSON.stringify(schemas[0]);
-        		if (obj.length == 1 || schemas.every(function (e) { return JSON.stringify(e) == str_schema_0; }))
-        			retschema = [ schemas[0] ];
-        		else
-        			retschema = schemas;
-        	}
+        	var str_schema_0 = JSON.stringify(schemas[0]);
+        	if (obj.length == 1 || schemas.every(function (e) { return JSON.stringify(e) == str_schema_0; }))
+        	    retschema = [ schemas[0] ];
+        	else
+        	    retschema = schemas;
+            }
         } // null
         else if (obj === null)
         {
@@ -76,10 +77,10 @@ function()
         // Other
         else if ((typeof obj) == 'number')
         {
-        	retschema = 0;
+            retschema = 0;
         } else if ((typeof obj == 'boolean'))
         {
-        	retschema = true;
+            retschema = true;
       	} else if ((typeof obj == 'string'))
         {
             retschema = 's';
