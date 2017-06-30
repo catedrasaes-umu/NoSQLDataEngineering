a7562d32b3610c8ff401d1b2b6469d2cdb8de091
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-06-30 02:16:30 +0200

    Adding union type creation for Mongoose.

---
+++ es.um.nosql.schemainference.entitydifferentiation/npm2/util/UnionType.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+function make_union_type(name, type1, type2)
+{
+    var capitalized_name = name.charAt(0).toUpperCase() + name.slice(1);
+    var typefn =
+        new Function(key, options,
+                     "mongoose.SchemaType.call(this, key, options, '"
+                     + capitalized_name
+                     + "');");
+    typefn.prototype = Object.create(mongoose.SchemaType.prototype);
+
+
+
+    // Add to mongoose registry
+    mongoose.Schema.Types[capitalized_name] = typefn;
+}
+
+
+
+module.exports = make_union_type;
