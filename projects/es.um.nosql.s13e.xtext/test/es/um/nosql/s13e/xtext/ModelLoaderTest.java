31e82ca3c1b348b20d1a3ed3430d23ef74e6316e
Alberto Hernández <aherchillon1988@hotmail.com>
2018-10-03 23:23:02 +0200

    Finally a XText ModelLoader. Reminder: MWE2 is PURE garbage

--- projects/es.um.nosql.s13e.xtext/test/es/um/nosql/s13e/xtext/ModelLoaderTest.java
+++ projects/es.um.nosql.s13e.xtext/test/es/um/nosql/s13e/xtext/ModelLoaderTest.java
@@ -1,55 +1,38 @@
 package es.um.nosql.s13e.xtext;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.resource.XtextResourceSet;
-import org.junit.Before;
 import org.junit.Test;
 
-import com.google.inject.Injector;
 
 import es.um.nosql.s13e.NoSQLSchema.NoSQLSchema;
 import es.um.nosql.s13e.NoSQLSchema.NoSQLSchemaPackage;
-import es.um.nosql.s13e.util.ModelLoader;
+import es.um.nosql.s13e.entitydifferentiation.EntityDifferentiation.EntityDifferentiation;
+import es.um.nosql.s13e.entitydifferentiation.EntityDifferentiation.EntityDifferentiationPackage;
 import es.um.nosql.s13e.util.compare.CompareNoSQLSchema;
+import es.um.nosql.s13e.xtext.util.ModelLoader;
 
 public class ModelLoaderTest
 {
   private String route1 = "../es.um.nosql.models/mongosongs/mongosongs.xmi";
   private String route2 = "models/test1.nosqlschema";
-
-  @Before
-  public void setUp()
-  {
-  }
+  private String route3 = "../es.um.nosql.models/facebook/facebook_Diff.xmi";
 
   @Test
-  public void testModelLoader()
+  public void testUnifiedModelLoader()
   {
-    Injector injector = new NoSQLSchemaStandaloneSetup().createInjectorAndDoEMFRegistration();
-    XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
-
-    resourceSet.getPackageRegistry().put(NoSQLSchemaPackage.eINSTANCE.getNsURI(), NoSQLSchemaPackage.eINSTANCE);
-
-    resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-    Resource resource = resourceSet.getResource(URI.createFileURI(route1), true);
-
-    NoSQLSchema schema1 = (NoSQLSchema)resource.getContents().get(0);
-
-
     ModelLoader loader = new ModelLoader(NoSQLSchemaPackage.eINSTANCE);
-    NoSQLSchema schema2 = loader.load(new File(route1), NoSQLSchema.class);
-
-//    ModelLoader loader = new ModelLoader(NoSQLSchemaPackage.eINSTANCE);
-//    NoSQLSchema nosqlschema = loader.load(new File(route2), NoSQLSchema.class);
+    NoSQLSchema schema1 = loader.load(new File(route1), NoSQLSchema.class);
+    es.um.nosql.s13e.util.ModelLoader loader2 = new ModelLoader(NoSQLSchemaPackage.eINSTANCE);
+    NoSQLSchema schema2 = loader2.load(new File(route1), NoSQLSchema.class);
 
     assertTrue(new CompareNoSQLSchema().compare(schema1, schema2));
+    assertNotNull(loader.load(new File(route2), NoSQLSchema.class));
+
+    ModelLoader loader3 = new ModelLoader(EntityDifferentiationPackage.eINSTANCE);
+    assertNotNull(loader3.load(new File(route3), EntityDifferentiation.class));
   }
 }
