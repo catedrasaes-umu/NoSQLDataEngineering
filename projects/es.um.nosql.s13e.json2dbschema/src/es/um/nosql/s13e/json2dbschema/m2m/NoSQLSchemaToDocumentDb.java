4bf5c44685ed02c899427dc43a634ed692fc4ed4
Alberto Hernández <aherchillon1988@hotmail.com>
2018-10-07 23:06:52 +0200

    TODO

---
+++ projects/es.um.nosql.s13e.json2dbschema/src/es/um/nosql/s13e/json2dbschema/m2m/NoSQLSchemaToDocumentDb.java
@@ -0,0 +1,8 @@
+package es.um.nosql.s13e.json2dbschema.m2m;
+
+public class NoSQLSchemaToDocumentDb
+{
+  //TODO: Remove ReferenceClass
+  //TODO: Remove Reference to StructuralVariation
+  //TODO: Remove PMap
+}
