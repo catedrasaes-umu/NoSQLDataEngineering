acb214d92eef40a9502e1d8f7f805022ab31f1aa
Alberto Hernández <alberto.hernandez1@um.es>
2019-04-07 18:58:15 +0200

    Python

--- projects/es.um.nosql.s13e.evolution/python/src/main.py
+++ projects/es.um.nosql.s13e.evolution/python/src/main.py
@@ -1,9 +1,14 @@
+import sys
 from charts.GanttChart import GanttChart
 
-CSV_ROUTE = "../output/reddit/reddit.csv"
+REDDIT_CSV_ROUTE = "../../output/reddit/reddit.csv"
+SOF_CSV_ROUTE = "../../output/stackoverflow/stackoverflow.csv"
+SOF_CSV_OUTLIERS_ROUTE = "../../output/stackoverflow/stackoverflow_outliers.csv"
+SOF_CSV_LIVEVARS_ROUTE = "../../output/stackoverflow/stackoverflow_livevars.csv"
 
 def main():
-  chartCreator = GanttChart(CSV_ROUTE)
+
+  chartCreator = GanttChart(REDDIT_CSV_ROUTE)
   chartCreator.showCharts()
 
 main()
